caaab784b9a94f365cddb7f3f67ead3000953011
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-04-08 16:37:52 -0700

    Simplify git operations

--- sync.sh
+++ sync.sh
@@ -3,11 +3,10 @@
 
 github_sync() {
     local now=$(date --iso-8601=seconds)
-    git add -u
-    git commit -m "WIP - ${now}"
+    git commit -am "WIP - ${now}"
     rc=$?; if [[ ${rc} == 0 ]]; then
         git push -f
-        git reset --soft HEAD^
+        git reset HEAD^
     fi
 }
 
