5de3319abeb77ecb32a6bd2b54267509be5bdd53
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-04-08 16:56:07 -0700

    Add diff check to avoid unnecessary work

--- sync.sh
+++ sync.sh
@@ -3,10 +3,12 @@
 
 github_sync() {
     local now=$(date --iso-8601=seconds)
-    git commit -am "WIP - ${now}"
-    rc=$?; if [[ ${rc} == 0 ]]; then
-        git push -f
-        git reset HEAD^
+
+    git diff --exit-code > /dev/null; if [ $? -ne 0 ]; then
+        git commit -am "WIP - ${now}"; if [ $? -eq 0 ]; then
+            git push -f
+            git reset HEAD^
+        fi
     fi
 }
 
