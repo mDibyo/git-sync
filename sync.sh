80e6772046c20dc573af5d2e58f983e35cfda195
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-04-08 17:03:17 -0700

    Clean up output from tool

--- sync.sh
+++ sync.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 
-github_sync() {
+function git_sync() {
     local now=$(date --iso-8601=seconds)
 
     git diff --exit-code > /dev/null; if [ $? -ne 0 ]; then
-        git commit -am "WIP - ${now}"; if [ $? -eq 0 ]; then
-            git push -f
-            git reset HEAD^
+        git commit -am "WIP - ${now}" > /dev/null; if [ $? -eq 0 ]; then
+            git push -f 2> /dev/null && echo "Synced ${now}"
+            git reset HEAD^ > /dev/null
         fi
     fi
 }
 
 while :
 do
-    inotifywait -r -e modify .
-    github_sync
+    inotifywait -rq -e modify .
+    git_sync
 done
