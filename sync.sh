4cb9fc30362516eb7087d995d2e7b1ec36c173a6
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-04-08 18:54:06 -0700

    Add unsyncing at end

--- sync.sh
+++ sync.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
 
-function git_sync() {
+function git_sync {
     local now=$(date --iso-8601=seconds)
 
     git diff --exit-code > /dev/null; if [ $? -ne 0 ]; then
         git commit -am "WIP - ${now}" > /dev/null; if [ $? -eq 0 ]; then
-            git push -f 2> /dev/null && echo "Synced ${now}"
+            git push -f 2> /dev/null && echo "Synced ${now}" || exit $?
             git reset HEAD^ > /dev/null
         fi
     fi
 }
 
-while :
-do
-    inotifywait -rq -e modify .
-    git_sync
-done
+function unsync {
+    echo "Unsyncing..."
+    git push -f 2> /dev/null
+}
+
+function main {
+    trap unsync SIGHUP SIGINT SIGTERM
+    git_sync || exit $?
+    while :; do
+        inotifywait -rq --exclude \.git --event modify . || exit $?
+        git_sync || exit $?
+    done
+}
+
+
+main
